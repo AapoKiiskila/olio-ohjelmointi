ba06b0fe035217e106d93bd7d8824427896bc304
Aapo Kiiskilä <aapo.kiiskila@gmail.com>
2024-01-25 11:20:33 +0200

    tehtävä 3 vaihe 3

--- teht3/italianchef.cpp
+++ teht3/italianchef.cpp
@@ -1,1 +1,17 @@
 #include "italianchef.h"
+
+
+ItalianChef::ItalianChef(string newName) : Chef(newName)
+{
+    cout<<"Chef "<<name<<" konstruktori"<<endl;
+}
+
+string ItalianChef::getName()
+{
+    return name;
+}
+
+void ItalianChef::makePasta()
+{
+    cout<<"Chef "<<name<<" makes pasta"<<endl;
+}
--- teht3/main.cpp
+++ teht3/main.cpp
@@ -1,14 +1,13 @@
 #include "chef.h"
+#include "italianchef.h"
 
 int main()
 {
-    Chef objectChef1("Gordon Ramsay");
-    objectChef1.makeSalad();
-    objectChef1.makeSoup();
-
-    Chef objectChef2("Anthony Bourdain");
-    objectChef2.makeSalad();
-    objectChef2.makeSoup();
+    Chef objectChef3("Jyrki");
+    ItalianChef objectChef4("Mario");
+    objectChef3.makeSalad();
+    objectChef4.makePasta();
+    objectChef3.makeSoup();
 
     return 0;
 }
